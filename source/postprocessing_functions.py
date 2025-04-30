93b15d32d5dd54d8500259341dbe04ca67098d8d
Pablo Gil <pablo.gilgonzalez00@gmail.com>
2025-05-01 01:26:30 +0200

    Implemented function read_simulation_data. Defined format to save simulation results as python variables as joblib.

--- source/postprocessing_functions.py
+++ source/postprocessing_functions.py
@@ -7,7 +7,7 @@ associated program (at time of writing, not yet included as a GitHub
 project, but it will be!).
 
 This module requires the module Ofpp (OpenFOAM Post Processing).
-It also uses os, subprocess, warnings
+It also uses os, subprocess, warnings, joblib
 
 """
 
@@ -17,6 +17,7 @@ import time
 import os
 import subprocess
 import warnings
+import joblib
 
 class CaseParameters:
     """Container class to store the parameters of a simulation.
@@ -297,11 +298,11 @@ def get_timestep_folders(case_path: str) -> list[str]:
 
     # Check if the list is empty or if it only contains the '0' 
     # folder:
-    if len(timestep_folders) is 0:
+    if len(timestep_folders) == 0:
         warnmessage = "No timestep folders found in " + case_path + " not even '0' folder"
         warnings.warn(warnmessage)
 
-    elif (len(timestep_folders) is 1) and (float(timestep_folders[0]) == 0.0):
+    elif (len(timestep_folders) == 1) and (float(timestep_folders[0]) == 0.0):
         warnmessage = "No timestep folders found in " + case_path + " just '0' folder"
         warnings.warn(warnmessage)
 
@@ -310,49 +311,170 @@ def get_timestep_folders(case_path: str) -> list[str]:
 
 
 
-def read_simulation_data(case_path: str, fluid_region_name: str = 'FluidRegion/', solid_region_name: str = 'CatalystRegion/'):
+def read_simulation_data(   case_path: str, 
+                            fluid_region_name: str = 'FluidRegion/', 
+                            solid_region_name: str = 'CatalystRegion/', 
+                            momentum_simulation_folder: str = 'MomentumSolution/',
+                            config_file_name: str = 'caseConfig.sh' 
+                        ):
     """Function for reading the simulation data (mesh and results)
 
+    This function reads the simulation files and saves the 
+    information in Python objects and lists and saves the data in a 
+    joblib file in the same simulation directory.
+
+    Parameters
+    ----------
+    case_path : str
+        Relative path to the case folder
+    fluid_region_name : str
+        Name of the fluid region in the OpenFOAM simulation. By 
+        default 'FluidRegion/'
+    solid_region_name : str
+        Name of the solid region in the OpenFOAM simulation. By 
+        default 'CatalystRegion/'
+    momentum_simulation_folder : str
+        Momentum and heat simulations are decoupled. Name of the 
+        direrctory (inside case_path) where the momentum simulation 
+        files are. By default 'MomentumSolution/'
+    config_file_name : str
+        Name of the case config file (default name 'caseConfig.sh')
+
+    Returns Nothing
+    ---------------
+
+    Results
+    -------
+    joblilb file
+        This function creates a joblib file in the case folder 
+        containing the data extracted from the simulation. Data in 
+        that file is stored in a dictionar. To load it in a script 
+        run:
+            dict = joblib.load('path_to_the_joblib_file.joblib')
+        The dict contains the following items (identified by their 
+        key):
+
+        "Parameters" : CaseParameters
+            Object of class CaseParameters containing the values 
+            of the parameters of this simulaton
+        "SolidMesh" : Ofpp.mesh_parser.FoamMesh
+            Mesh of the solid region of the catalyst (Ofpp class)
+        "FluidMesh" : Ofpp.mesh_parser.FoamMesh
+            Mesh of the fluid region of the catalyst (Ofpp class)
+        "MomentumTimesteps" : list[str]
+            List with the timesteps of the momentum simulation (as 
+            strings, the names of the folders containing each 
+            timestep. MomentumTimesteps[0]='0' is always the '0' 
+            folder of the OpenFOAM simulation.)
+        "ThermalTimesteps" : list[str]
+            List with the timesteps of the temperature simulation (as
+            strings, the names of the folders containing each 
+            timestep. MomentumTimesteps[0]='0' is always the '0' 
+            folder of the OpenFOAM simulation).
+        "FluidTemperature" : list[list[numpy.float64]]
+            List "FluidTemperature" contains one list for each 
+            timestep in "ThermalTimesteps" in the same order 
+            starting for the second time step (the first is the 
+            initial condition).
+            For each time step, it contains a list with the 
+            temperature at the center of every cell in the fluid 
+            region. The cells are ordered in the same way as in 
+            fluidmesh.cell_centres
+        "SolidTemperature" : list[list[numpy.float64]]
+            Same as "FluidTemperature" but for the solid region.
+        "StaticPressure" : list[list[numpy.float64]]
+            Same as "FluidTemperature" but for static pressure instead
+            of temperature.
+        "VelocityField" list[list[numpy.ndarray]]: 
+            Same as "FluidTemperature" but for velocity pressure 
+            instead of temperature. Since velocity is a vector, for 
+            every cell center instead of a float we have a 
+            numpy.ndarray with three elements.
 
     """
 
-    time1 = time.time()
     # Read the fluid region mesh:
+    time1 = time.time()
     fluidmesh = Ofpp.FoamMesh(case_path, fluid_region_name)
 
     time2 = time.time()
     print("Time required for reading the fluidmesh = ", time2-time1)
 
-    time2_1 = time.time()
     # Read the coordinates of the centers of cells and their volumes
     # For this function to work user must have been generated the 'C'
     # and 'V' mesh files for each region. These files are  created by 
     # OpenFOAM's own postprocessing utilities.
     fluidmesh.read_cell_centres(case_path+'0/'+fluid_region_name+'C')
     fluidmesh.read_cell_volumes(case_path+'0/'+fluid_region_name+'V')
-    time2_2 = time.time()
 
-    print("Time reading fluidmesh centres and volumes = ", time2_2-time2_1)
-
-    
     # Read the solid region mesh:
+    time3 = time.time()
     solidmesh = Ofpp.FoamMesh(case_path, solid_region_name)
 
-    time3 = time.time()
-    print("Time required for reading the solidmesh = ", time3-time2)
+    time4 = time.time()
+    print("Time required for reading the solidmesh = ", time4-time3)
 
-    time3_1 = time.time()
     # Read the coordinates of the centers of cells and their volumes
     solidmesh.read_cell_centres(case_path+'0/'+solid_region_name+'C')
     solidmesh.read_cell_volumes(case_path+'0/'+solid_region_name+'V')
-    time3_2 = time.time()
 
-    print("Time reading solidmesh centres and volumes = ", time3_2-time3_1)
+    # Since the simulations for momentum and temperature are decoupled,
+    # we have different timesteps for each of the simulations:
+    timesteps_momentum = get_timestep_folders(case_path + 'MomentumSolution/')
+    timesteps_thermal = get_timestep_folders(case_path)
+    print("Timesteps momentum simulation: ",timesteps_momentum)
+    print("Timesteps thermal simulation: ",timesteps_thermal)
 
-    timesteps = get_timestep_folders(case_path)
-    print(timesteps)
+    # Read the magnitudes for each timestep:
+    # TODO  Write the option to read just some of the timesteps and 
+    #       some of the magnitudes.
+
+    # Timestep [0] is the '0' folder, skip that one.
+    timesteps_to_read_thermal = timesteps_thermal[1:]
+    timesteps_to_read_momentum = timesteps_momentum[1:]
+
+    # Parse each field for each timestep and store them in a list.
+    #  Each element of the list contains the value of the field in 
+    # the timestep that is in the same position in the 
+    # 'timesteps_to_read_*' list.
+    T_fluid = []
+    T_solid = []
+    for ts in timesteps_to_read_thermal:
+        T_fluid.append(Ofpp.parse_internal_field(case_path + ts + '/' + fluid_region_name + 'T'))
+        T_solid.append(Ofpp.parse_internal_field(case_path + ts + '/' + solid_region_name + 'T'))
+
+    static_p = []
+    vel_field = []
+    for ts in timesteps_to_read_momentum:
+        static_p.append(Ofpp.parse_internal_field(case_path + momentum_simulation_folder + ts + '/' + 'static(p)'))
+        vel_field.append(Ofpp.parse_internal_field(case_path + momentum_simulation_folder + ts + '/' + 'U'))
+
+    # Save the data in a joblib file. The rationale behind this is 
+    # that reading and parsing data from the OpenFOAM files can take 
+    # quite a long time, up to a couple minutes. If we save all 
+    # those files in one of these fast formats I can read the data 
+    # from openfoam once and then use them as I want (may not sound 
+    # very convincing, but trust me I've spent a lot of very 
+    # frustating time waiting for these data to load). 
+
+    # Include the case parameters too:
+    params = load_case_properties(case_path,  config_file_name)
+    # Save the variables as a dict:
+    sim_data = {"Parameters": params,
+                "SolidMesh": solidmesh, 
+                "FluidMesh": fluidmesh, 
+                "MomentumTimesteps": timesteps_momentum,
+                "ThermalTimesteps": timesteps_thermal,
+                "FluidTemperature": T_fluid,
+                "SolidTemperature": T_solid,
+                "StaticPressure": static_p,
+                "VelocityField": vel_field}
+
+    datafilename = case_path + "Case_P" + str(params.porosity) + "_Re" + str(params.Rep) + "_DATA.joblib"
+
+    joblib.dump(sim_data, datafilename)
 
 
-    return True
+
 
 
